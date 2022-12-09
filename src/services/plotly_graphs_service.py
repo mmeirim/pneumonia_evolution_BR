f7384e0c4b8930979e80e4a110f48d4f9d8d7382
matheus.meirim <matheus.meirim@b2wdigital.com>
2022-12-09 02:36:46 -0300

    feat: improve streamlit dash

--- src/services/plotly_graphs_service.py
+++ src/services/plotly_graphs_service.py
@@ -4,35 +4,279 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 
-def graph_pneumocom_percent_of_general_admissions():
+def graph_pneumocom_lethality():
     years = ["2011","2012","2013","2014","2015","2016","2017","2018","2019", "2020", "2021"]
-    admissoes_total = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',0, header=None, nrows=1, usecols="B:J,P:Q",skiprows=12,thousands=',')
+
+    admissoes_total = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',3, header=None, nrows=1, usecols="B:J,P:Q",skiprows=22,thousands=',')
     admissoes_total = admissoes_total.astype(float)
-    admissoes_total = admissoes_total.T
-    admissoes_total.columns = ["count"]
-    admissoes_total["year"] = ["2011","2012","2013","2014","2015","2016","2017","2018","2019", "2020", "2021"]
 
-    admissoes_total_tx = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',0, header=None, nrows=1, usecols="B:J,P:Q",skiprows=12,thousands=',')
-    admissoes_total_tx = admissoes_total_tx.astype(float)
+    admissoes_non_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',3, header=None, nrows=1, usecols="B:J,P:Q",skiprows=37,thousands=',')
+    admissoes_non_elderly = admissoes_non_elderly.astype(float)
 
-    admissoes_gerais_total_tx = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',0, header=None, nrows=1, usecols="B:J,P:Q",skiprows=11,thousands=',')
-    admissoes_gerais_total_tx = admissoes_gerais_total_tx.astype(float)
+    admissoes_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',3, header=None, nrows=1, usecols="B:J,P:Q",skiprows=40,thousands=',')
+    admissoes_elderly = admissoes_elderly.astype(float)
 
-    pneumonia_percent_total_tx = round((admissoes_total_tx/admissoes_gerais_total_tx)*100,1)
+    admissoes_icu = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',3, header=None, nrows=1, usecols="B:J,P:Q",skiprows=23,thousands=',')
+    admissoes_icu = admissoes_icu.astype(float)
 
-    pneumonia_percent_total_transposed = pneumonia_percent_total_tx.T
-    pneumonia_percent_total_transposed.columns = ["percent"]
-    pneumonia_percent_total_transposed["year"] = ["2011","2012","2013","2014","2015","2016","2017","2018","2019", "2020", "2021"]
+    admissoes_icu_non_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',3, header=None, nrows=1, usecols="B:J,P:Q",skiprows=38,thousands=',')
+    admissoes_icu_non_elderly = admissoes_icu_non_elderly.astype(float)
 
-    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    admissoes_icu_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',3, header=None, nrows=1, usecols="B:J,P:Q",skiprows=41,thousands=',')
+    admissoes_icu_elderly = admissoes_icu_elderly.astype(float)
+
+    letalidade_icu = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',4, header=None, nrows=1, usecols="B:J,P:Q",skiprows=15,thousands=',')
+    letalidade_icu = round(letalidade_icu.astype(float)*100,1)
+    letalidade_icu = letalidade_icu.T
+    letalidade_icu.columns = ["count"]
+    letalidade_icu["year"] = years
+
+    letalidade_icu_non_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',4, header=None, nrows=1, usecols="B:J,P:Q",skiprows=20,thousands=',')
+    letalidade_icu_non_elderly = round(letalidade_icu_non_elderly.astype(float)*100,1)
+    letalidade_icu_non_elderly = letalidade_icu_non_elderly.T
+    letalidade_icu_non_elderly.columns = ["count"]
+    letalidade_icu_non_elderly["year"] = years
+
+    letalidade_icu_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',4, header=None, nrows=1, usecols="B:J,P:Q",skiprows=21,thousands=',')
+    letalidade_icu_elderly = round(letalidade_icu_elderly.astype(float)*100,1)
+    letalidade_icu_elderly = letalidade_icu_elderly.T
+    letalidade_icu_elderly.columns = ["count"]
+    letalidade_icu_elderly["year"] = years
+
+    letalidade_non_icu = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',4, header=None, nrows=1, usecols="B:J,P:Q",skiprows=8,thousands=',')
+    letalidade_non_icu = round(letalidade_non_icu.astype(float)*100,1)
+    letalidade_non_icu = letalidade_non_icu.T
+    letalidade_non_icu.columns = ["count"]
+    letalidade_non_icu["year"] = years
+
+    letalidade_non_icu_non_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',4, header=None, nrows=1, usecols="B:J,P:Q",skiprows=13,thousands=',')
+    letalidade_non_icu_non_elderly = round(letalidade_non_icu_non_elderly.astype(float)*100,1)
+    letalidade_non_icu_non_elderly = letalidade_non_icu_non_elderly.T
+    letalidade_non_icu_non_elderly.columns = ["count"]
+    letalidade_non_icu_non_elderly["year"] = years
+
+    letalidade_non_icu_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',4, header=None, nrows=1, usecols="B:J,P:Q",skiprows=14,thousands=',')
+    letalidade_non_icu_elderly = round(letalidade_non_icu_elderly.astype(float)*100,1)
+    letalidade_non_icu_elderly = letalidade_non_icu_elderly.T
+    letalidade_non_icu_elderly.columns = ["count"]
+    letalidade_non_icu_elderly["year"] = years
+
+
+    total_percent = round((admissoes_icu/admissoes_total)*100,1)
+
+    total_percent_transposed = total_percent.T
+    total_percent_transposed.columns = ["percent"]
+    total_percent_transposed["non-elderly"] = round((admissoes_icu_non_elderly/admissoes_non_elderly)*100,1).T
+    total_percent_transposed["elderly"] = round((admissoes_icu_elderly/admissoes_elderly)*100,1).T
+    total_percent_transposed["year"] = years
+
+    fig = make_subplots(rows=1, cols=3, subplot_titles=("All", "Non elderly", "Elderly"), specs=[[{"secondary_y": True},{"secondary_y": True},{"secondary_y": True}]])
     fig.add_trace( 
-        go.Bar(x = years,y =admissoes_total["count"],name="Admissions",),
+        go.Bar(x = years,y =total_percent_transposed["percent"],name="ICU occupation (%)",legendgroup="group1",legendgrouptitle_text="First Graph"),
         secondary_y=False,
+        row=1, col=1,
     )
 
     fig.add_trace(
-        go.Line(x = years,y = pneumonia_percent_total_transposed["percent"],name="Admissions_Percent"),
+        go.Line(x = years,y = letalidade_icu["count"],name="ICU Lethality",legendgroup="group1"),
         secondary_y=True,
+        row=1, col=1,
+    )
+
+    fig.add_trace(
+        go.Line(x = years,y = letalidade_non_icu["count"],name="Non ICU Lethality",legendgroup="group1"),
+        secondary_y=True,
+        row=1, col=1,
+    )
+
+    fig.add_trace( 
+        go.Bar(x = years,y =total_percent_transposed["non-elderly"],name="ICU occupation (%)",legendgroup="group2",legendgrouptitle_text="Second Graph"),
+        secondary_y=False,
+        row=1, col=2,
+    )
+
+    fig.add_trace(
+        go.Line(x = years,y = letalidade_icu_non_elderly ["count"],name="ICU Lethality",legendgroup="group2"),
+        secondary_y=True,
+        row=1, col=2,
+    )
+
+    fig.add_trace(
+        go.Line(x = years,y = letalidade_non_icu_non_elderly ["count"],name="Non ICU Lethality",legendgroup="group2"),
+        secondary_y=True,
+        row=1, col=2,
+    )
+
+    fig.add_trace( 
+        go.Bar(x = years,y =total_percent_transposed["elderly"],name="ICU occupation (%)",legendgroup="group3", legendgrouptitle_text="Third Graph",),
+        secondary_y=False,
+        row=1, col=3,
+    )
+
+    fig.add_trace(
+        go.Line(x = years,y = letalidade_icu_elderly["count"],name="ICU Lethality",legendgroup="group3"),
+        secondary_y=True,
+        row=1, col=3,
+    )
+
+    fig.add_trace(
+        go.Line(x = years,y = letalidade_non_icu_elderly["count"],name="Non ICU Lethality",legendgroup="group3"),
+        secondary_y=True,
+        row=1, col=3,
+    )
+
+    fig.update_layout(
+        title_text="Pneumonia ICU occupation and lethality by age group",
+        template='simple_white',
+        xaxis = dict(
+            tickmode = 'linear',
+            tick0 = 2011,
+            dtick = 1
+        ),
+        legend = dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.12,
+            xanchor="right",
+            x=1
+        )
+    )
+
+    fig.update_xaxes(title_text="Year")
+    fig.update_yaxes(title_text="<b>Pneumonia ICU occupation percentage</b> ", secondary_y=False,)
+    fig.update_yaxes(title_text="<b>Pneumonia Lethality rate</b>", secondary_y=True)
+
+    return fig
+
+def graph_pneumocom_admissions():
+    years = ["2011","2012","2013","2014","2015","2016","2017","2018","2019", "2020", "2021"]
+
+    admissoes_total = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',2, header=None, nrows=1, usecols="B:J,P:Q",skiprows=8,thousands=',')
+    admissoes_total = admissoes_total.astype(float)
+    admissoes_total = admissoes_total.T
+    admissoes_total.columns = ["count"]
+    admissoes_total["year"] = years
+
+    admissoes_non_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',2, header=None, nrows=1, usecols="B:J,P:Q",skiprows=13,thousands=',')
+    admissoes_non_elderly = admissoes_non_elderly.astype(float)
+    admissoes_non_elderly = admissoes_non_elderly.T
+    admissoes_non_elderly.columns = ["count"]
+    admissoes_non_elderly["year"] = years
+
+    admissoes_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',2, header=None, nrows=1, usecols="B:J,P:Q",skiprows=14,thousands=',')
+    admissoes_elderly = admissoes_elderly.astype(float)
+    admissoes_elderly = admissoes_elderly.T
+    admissoes_elderly.columns = ["count"]
+    admissoes_elderly["year"] = years
+
+
+    fig = make_subplots()
+
+    fig.add_trace(
+        go.Line(x = years,y = admissoes_total["count"],name="All"),
+    )
+
+    fig.add_trace(
+        go.Line(x = years,y = admissoes_non_elderly["count"],name="Non Elderly"),
+    )
+
+    fig.add_trace(
+        go.Line(x = years,y = admissoes_elderly["count"],name="Elderly"),
+    )
+
+    fig.add_vline(x=8, line_width=1, line_dash="dash", line_color="black")
+
+
+    fig.update_layout(
+        title_text="Pneumonia Age-adjusted admissions",
+        template='simple_white',
+        xaxis = dict(
+            tickmode = 'linear',
+            tick0 = 2011,
+            dtick = 1
+        ),
+    )
+
+    fig.update_xaxes(title_text="Year")
+    fig.update_yaxes(title_text="<b>Age-adjusted Pneumonia admissions rate</b> ")
+
+    return fig
+
+def graph_pneumocom_percent_of_general_admissions():
+    years = ["2011","2012","2013","2014","2015","2016","2017","2018","2019", "2020", "2021"]
+    admissoes_total = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',0, header=None, nrows=1, usecols="B:J,P:Q",skiprows=16,thousands=',')
+    admissoes_total = admissoes_total.astype(float)
+
+    admissoes_non_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',0, header=None, nrows=1, usecols="B:J,P:Q",skiprows=26,thousands=',')
+    admissoes_non_elderly = admissoes_non_elderly.astype(float)
+
+    admissoes_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',0, header=None, nrows=1, usecols="B:J,P:Q",skiprows=28,thousands=',')
+    admissoes_elderly = admissoes_elderly.astype(float)
+
+    admissoes_gerais_total_tx = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',0, header=None, nrows=1, usecols="B:J,P:Q",skiprows=15,thousands=',')
+    admissoes_gerais_total_tx = admissoes_gerais_total_tx.astype(float)
+
+    admissoes_gerais_non_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',0, header=None, nrows=1, usecols="B:J,P:Q",skiprows=25,thousands=',')
+    admissoes_gerais_non_elderly = admissoes_gerais_non_elderly.astype(float)
+
+    admissoes_gerais_elderly = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',0, header=None, nrows=1, usecols="B:J,P:Q",skiprows=27,thousands=',')
+    admissoes_gerais_elderly = admissoes_gerais_elderly.astype(float)
+
+    pneumonia_percent_total_tx = round((admissoes_total/admissoes_gerais_total_tx)*100,1)
+
+    pneumonia_percent_total_transposed = pneumonia_percent_total_tx.T
+    pneumonia_percent_total_transposed.columns = ["percent"]
+    pneumonia_percent_total_transposed["non-elderly"] = round((admissoes_non_elderly/admissoes_gerais_non_elderly)*100,1).T
+    pneumonia_percent_total_transposed["elderly"] = round((admissoes_elderly/admissoes_gerais_elderly)*100,1).T
+    pneumonia_percent_total_transposed["year"] = years
+
+    admissoes_total = admissoes_total.T
+    admissoes_total.columns = ["count"]
+    admissoes_total["year"] = years
+
+    admissoes_non_elderly = admissoes_non_elderly.T
+    admissoes_non_elderly.columns = ["count"]
+    admissoes_non_elderly["year"] = years
+
+    admissoes_elderly = admissoes_elderly.T
+    admissoes_elderly.columns = ["count"]
+    admissoes_elderly["year"] = years
+    
+    fig = make_subplots(rows=1, cols=3, subplot_titles=("All", "Non elderly", "Elderly"), specs=[[{"secondary_y": True},{"secondary_y": True},{"secondary_y": True}]])
+    fig.add_trace( 
+        go.Bar(x = years,y =admissoes_total["count"],name="All",legendgroup="group1",legendgrouptitle_text="First Graph"),
+        secondary_y=False,
+        row=1, col=1,
+    )
+
+    fig.add_trace(
+        go.Line(x = years,y = pneumonia_percent_total_transposed["percent"],name="All (%)",legendgroup="group1"),
+        secondary_y=True,
+        row=1, col=1,
+    )
+
+    fig.add_trace( 
+        go.Bar(x = years,y =admissoes_non_elderly["count"],name="Non Elderly",legendgroup="group2",legendgrouptitle_text="Second Graph"),
+        secondary_y=False,
+        row=1, col=2,
+    )
+
+    fig.add_trace(
+        go.Line(x = years,y = pneumonia_percent_total_transposed["non-elderly"],name="Non Elderly (%)",legendgroup="group2"),
+        secondary_y=True,
+        row=1, col=2,
+    )
+
+    fig.add_trace( 
+        go.Bar(x = years,y =admissoes_elderly["count"],name="Elderly",legendgroup="group3", legendgrouptitle_text="Third Graph",),
+        secondary_y=False,
+        row=1, col=3,
+    )
+
+    fig.add_trace(
+        go.Line(x = years,y = pneumonia_percent_total_transposed["elderly"],name="Elderly (%)",legendgroup="group3"),
+        secondary_y=True,
+        row=1, col=3,
     )
 
     fig.update_layout(
@@ -43,10 +287,17 @@ def graph_pneumocom_percent_of_general_admissions():
             tick0 = 2011,
             dtick = 1
         ),
+        legend = dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.12,
+            xanchor="right",
+            x=1
+        )
     )
 
     fig.update_xaxes(title_text="Year")
-    fig.update_yaxes(title_text="<b>Age-adjusted Pneumonia admissions rate</b> ", secondary_y=False)
+    fig.update_yaxes(title_text="<b>Age-adjusted Pneumonia admissions rate</b> ", secondary_y=False,)
     fig.update_yaxes(title_text="<b>Percentage of Pneumonia cases</b>", secondary_y=True)
 
     return fig
--- src/streamlit_dash.py
+++ src/streamlit_dash.py
@@ -3,25 +3,8 @@ import pandas as pd
 
 import services.plotly_graphs_service as plotly_graphs_service
 
-BEGIN_YEAR = 2011
-LAST_YEAR = 2019
-STOP_YEAR = 2020
-
-def highlight_rows(x):
-    df = x.copy()
-    df.iloc[10:, 0] = 'background-color: yellow'
-    return df
-
 st.set_page_config(layout="wide")
-st.title("Pneumocom Dashboard") 
 
-st.header('Pneumocom and General Admissions by age group')
-st.plotly_chart(plotly_graphs_service.graph_pneumocom_percent_of_general_admissions(),use_container_width=True)
-
-data = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',0,nrows=20, usecols="A:L,P:S",thousands=',')
-
-years_choice = [2011,2012]
-# make_choice = st.radio('Select at least 2 years:',[2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021])
 make_choice = st.sidebar.multiselect('Select years to display on tables:',[2012,2013,2014,2015,2016,2017,2018])
 filter = ["Data Information",2011,2019,'%∆*','AAPC [95% CI]',2020,2021,'%∆*.1','AAPC [95% CI].1']
 for element in make_choice:
@@ -29,6 +12,109 @@ for element in make_choice:
         if isinstance(filter[i], int) and isinstance(filter[i+1], int):
             if element > filter[i] and element < filter[i+1]:
                 filter.insert(i+1,element)
+
+make_choice2 = st.sidebar.multiselect('Select age groups to display on tables:',['20-49','50-59','60-69','70+'])
+filter2 = ["Admissions","Non-Elderly","Elderly","Age-adjusted Admissions rate ᵃ","Deaths","Age-adjusted In-hospital\nMortality rate ᵃ",
+            "Age-adjusted In-hospital\nLethality rate","Age-adjusted Non ICU\nLethality rate","Age-adjusted ICU\nLethality rate"]
+for element in make_choice2:
+    for i in range(len(filter2)-1):
+        try:
+            el = int(element[:2])
+            first = int(filter2[i][:2])
+            second = int(filter2[i+1][:2])
+            if isinstance(first, int) and isinstance(second, int):
+                if el > first and el < second:
+                    filter2.insert(i+1,element)
+            if isinstance(first, int):
+                if el > first:
+                    filter2.insert(i+1,element)
+        except:
+            filter2.insert(1,element)
+
+
+
+st.title("Pneumocom Dashboard") 
+
+###########################################################################################################################
+st.header('Pneumonia Admissions by age group')
+
+st.plotly_chart(plotly_graphs_service.graph_pneumocom_admissions(),use_container_width=True)
+
+age_groups = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',2,nrows=28, usecols="A:L,P:S",thousands=',')
+st.dataframe(age_groups[age_groups["Data Information"].isin(filter2)][filter].style.format({
+    2011: '{:,.0f}'.format,
+    2012: '{:,.0f}'.format,
+    2013: '{:,.0f}'.format,
+    2014: '{:,.0f}'.format,
+    2015: '{:,.0f}'.format,
+    2016: '{:,.0f}'.format,
+    2017: '{:,.0f}'.format,
+    2018: '{:,.0f}'.format,
+    2019: '{:,.0f}'.format,
+    2020: '{:,.0f}'.format,
+    2021: '{:,.0f}'.format,
+    '%∆*' : '{:,.2f}'.format,
+    '%∆*.1' : '{:,.2f}'.format
+}),use_container_width=True)
+st.write("AAPC - Annual Average Percent Change (estimated from Poisson regression model")
+st.write("ᵃ Rates per 100,000 population, adjusted to the WHO standard population")
+st.write("*Percentage change between 2011-2019 and 2019-2021, respectively")
+
+###########################################################################################################################
+
+st.header('Pneumonia Lethality and ICU occupation by age group')
+
+st.plotly_chart(plotly_graphs_service.graph_pneumocom_lethality(),use_container_width=True)
+
+st.subheader('Pneumonia Lethality by age group')
+
+lethality = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',4,nrows=22, usecols="A:L,P:S",thousands=',')
+st.dataframe(lethality[lethality["Data Information"].isin(filter2)][filter].style.format({
+    2011: '{:.2%}'.format,
+    2012: '{:.2%}'.format,
+    2013: '{:.2%}'.format,
+    2014: '{:.2%}'.format,
+    2015: '{:.2%}'.format,
+    2016: '{:.2%}'.format,
+    2017: '{:.2%}'.format,
+    2018: '{:.2%}'.format,
+    2019: '{:.2%}'.format,
+    2020: '{:.2%}'.format,
+    2021: '{:.2%}'.format,
+    '%∆*' : '{:,.2f}'.format,
+    '%∆*.1' : '{:,.2f}'.format
+}),use_container_width=True)
+st.write("AAPC - Annual Average Percent Change (estimated from Poisson regression model")
+st.write("*Percentage change between 2011-2019 and 2019-2021, respectively")
+
+st.subheader('Pneumonia ICU occupation by age group')
+
+age_groups_icu = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',3,nrows=42, usecols="A:L,P:S",thousands=',')
+st.dataframe(age_groups_icu[filter].style.format({
+    2011: '{:,.0f}'.format,
+    2012: '{:,.0f}'.format,
+    2013: '{:,.0f}'.format,
+    2014: '{:,.0f}'.format,
+    2015: '{:,.0f}'.format,
+    2016: '{:,.0f}'.format,
+    2017: '{:,.0f}'.format,
+    2018: '{:,.0f}'.format,
+    2019: '{:,.0f}'.format,
+    2020: '{:,.0f}'.format,
+    2021: '{:,.0f}'.format,
+    '%∆*' : '{:,.2f}'.format,
+    '%∆*.1' : '{:,.2f}'.format
+}),use_container_width=True)
+st.write("AAPC - Annual Average Percent Change (estimated from Poisson regression model")
+st.write("ᵃ Rates per 100,000 population, adjusted to the WHO standard population")
+st.write("*Percentage change between 2011-2019 and 2019-2021, respectively")
+
+###########################################################################################################################
+
+st.header('Pneumocom vs General Admissions by age group')
+st.plotly_chart(plotly_graphs_service.graph_pneumocom_percent_of_general_admissions(),use_container_width=True)
+
+data = pd.read_excel('../tables/Pneumonia_data_by_age_group_08.11.xlsx',0,nrows=28, usecols="A:L,P:S",thousands=',')
 
 st.dataframe(data[filter].style.format({
     2011: '{:,.0f}'.format,
